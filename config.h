6109b08b8e2a040e9dae74d1d4f4388215d74908
alexandre <alex14fr@gmail.com>
2022-06-09 20:55:54 +0200

    x

--- config.h
+++ config.h
@@ -22,19 +22,18 @@
 // Path of the CA certificate database; usually a copy of /etc/ssl/cert.pem
 #define CADB "/cert.pem"
 
-// Upstream configuration; available presets:
+// Upstream configuration; available presets, or see below to set your custom parameters.
 //    GOOG_DOT    8.8.8.8 (Google), DNS over TLS
 //    GOOG_DOH    8.8.8.8 (Google), DNS over HTTPS
 //    CFLR_DOT    1.1.1.1 (Cloudflare), DNS over TLS
 //    CFLR_DOH    1.1.1.1 (Cloudflare), DNS over HTTPS
 // Preferer DNS over TLS if your network configuration allow TCP connections to port 853.
-// To define your own upstream parameters, comment the following line and see below.
-#define GOOG_DOT
+//#define GOOG_DOT
 
 // Customized upstream:
 //  #define IS_DOH   								// for DNS over HTTPS, leave undefined for DNS over TLS
-//  #define UPSTREAM_HOST "9.9.9.9:443"		// host IP:port as a string
-//  #define UPSTREAM_SRVNAME "quad9.net"  	// server subject name for certificate validation
+  #define UPSTREAM_HOST "9.9.9.9:853"			// host IP:port as a string
+  #define UPSTREAM_SRVNAME "dns.quad9.net"  	// server subject name for certificate validation
 //  #define DOH_PATH "/dns-query"				// path to use for DNS over HTTPS (leave undefined for DNS over TLS)
 
 
