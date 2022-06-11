cc422354d8addbffe7d16f7cc6e714362b471f53
alexandre <alex14fr@gmail.com>
2022-06-11 15:49:12 +0200

    x

--- dnscrypt/config.h
+++ dnscrypt/config.h
@@ -2,6 +2,8 @@
 // Edit as you need and recompile with make.
 //
 
+
+
 // Listen adress : INADDR_LOOPBACK, INADDR_ANY, inet_addr("192.168.1.1") 
 #define LISTEN_ADDR INADDR_LOOPBACK
 
@@ -22,30 +24,17 @@
 // Keep the hit count on each cache entry, leave undef to save a write to the DB at each request
 //#define CACHEDB_KEEP_HIT_COUNT
 
-// Upstream configuration; available presets, or see below to set your custom parameters.
+// Upstream configuration; available presets in public-resolvers.h, or see below to set your custom parameters.
 // See <https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v2/public-resolvers.md> for a list of sdns:// adresses
-// and <https://dnscrypt.info/stamps/> to convert the sdns:// stamp into suitable parameters.
-// Prefer UDP transport if your firewall allows it.
-#define UPSTREAM_SCALEWAY_PAR
-//#define UPSTREAM_ACSACSAR_AMS
+// Run `make public-resolvers.h' to get an up-to-date list.
+#define UPSTREAM_P0    // Set UPSTREAM_Pxx (choose xx in public-resolvers.h)
+#include "public-resolvers.h"
 
-#ifdef UPSTREAM_ACSACSAR_AMS   // hosted in Amsterdam, run by acsacsar
-#define UPSTREAM_HOST "51.158.166.97"			// host IP as a string
-#define UPSTREAM_PORT 443
-#define UPSTREAM_SRVNAME "\1" "2\xd" "dnscrypt-cert\xc" "acsacsar-ams\x3" "com\0"  	// provider name in DNS format
-#define UPSTREAM_PUBKEY "0327f3cf927e995f46fb2381e07c1c764ef25f5d8442ce48bdaee4577a06b651"	// long-term server pubkey (leave empty for no verification)
+// Prefer UDP transport if your firewall allows it, unset if it fails.
 #define UPSTREAM_UDP
-#endif
 
-#ifdef UPSTREAM_SCALEWAY_PAR  // hosted in Paris, run by Frank Denis, hosted by scaleway
-#define UPSTREAM_HOST "212.47.228.136"
-#define UPSTREAM_PORT 443
-#define UPSTREAM_SRVNAME "\1" "2\xd" "dnscrypt-cert\x2" "fr\x8" "dnscrypt\x3" "org\0"
-#define UPSTREAM_PUBKEY "e801b84ea606bfb0bac0ce43445bb15eba64b02fa3c4aa31ae10636a0790324d"
-#define UPSTREAM_UDP
-#endif
-
-#define TIMEOUT_SECS 2   // timeout in seconds for upstream answer
+// Timeout in seconds for upstream answer
+#define TIMEOUT_SECS 2
 
 // Debug options
 // #define DUMP_RTRIP     // show hexdump of the packets sent and received to/from upstream
@@ -54,3 +43,6 @@
 // #define DUMP_CACHE	  // dump cache hits and misses
 // #define DUMP_LOCAL	  // dump downstream packets 
 //
+//
+//
+
